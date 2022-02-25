216fbeeac0e779fb4257b840ed2d51689ef0d499
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-02-25 10:49:37 +0700

    use react-hook-form example to integrate custom input component

--- ROOK HOTEL/2_form_validation/src/App.tsx
+++ ROOK HOTEL/2_form_validation/src/App.tsx
@@ -1,9 +1,67 @@
-function App() {
+import React from "react";
+import { useForm } from "react-hook-form";
+
+export default function App() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }
+
+  console.log(watch("name")); // watch input value by passing the name of it
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Form onSubmit={onSubmit}>
+      <Input name='name' />
+      <Input name='birthDate' />
+      <Input name='country' />
+      <Input name='school' />
+      <Input name='study' />
+      <button>Submit</button>
+    </Form>
   );
 }
 
-export default App;
+const Input = (props: any) => {
+  const { register, name, ...rest } = props
+  return (
+    <div className="flex flex-col">
+      <label>{name}</label>
+      <input {...register(name)} {...rest} />
+    </div>
+  )
+}
+
+function Select({ register, options, name, ...rest }: any) {
+  return (
+    <select {...register(name)} {...rest}>
+      {options.map((value: any) => (
+        <option key={value} value={value}>
+          {value}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+function Form({ defaultValues, children, onSubmit }: any) {
+  const methods = useForm({ defaultValues });
+  const { handleSubmit } = methods;
+
+  return (
+    <div className="flex flex-col items-center bg-blue-200">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {React.Children.map(children, child => {
+          let result = child
+          if (child.props.name) {
+            result = React.createElement(child.type, {
+              ...child.props,
+              register: methods.register,
+              key: child.props.name
+            })
+          }
+          return result
+        })}
+      </form>
+    </div>
+  );
+}
