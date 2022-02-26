3ce2397b8967cd38ac96d26db19507a462dd4a34
Gibson Han <66423127+gibsonliketheguitar@users.noreply.github.com>
2022-02-26 12:06:39 +0700

    update select style and insert into form

--- ROOK HOTEL/2_form_validation/src/App.tsx
+++ ROOK HOTEL/2_form_validation/src/App.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 
 import { Form } from "./components/Form";
 import { Input } from "./components/Input";
+import Select from "./components/Select";
 import { TextArea } from "./components/TextArea";
 
 export default function App() {
@@ -9,8 +10,9 @@ export default function App() {
   const onSubmit = (data: any) => {
     console.log(data);
   }
-  const countries = require('./assets/data/country.json')
-  console.log(countries)
+  const countriesJSON = require('./assets/data/country.json')
+  const countriesName = countriesJSON.map((c: any) => c.name)
+
   console.log(watch("name")); // watch input value by passing the name of it
 
   return (
@@ -18,14 +20,14 @@ export default function App() {
       <Form onSubmit={onSubmit}>
         <Input type='text' name='name' label='Your full given name:' />
         <Input type='text' name='birthDate' label='Date of Birth' />
-        <Input type='text' name='country' label='Country of residence or citizenship:' />
+        <Select name="country" label='Country of residence or citizenship' options={countriesName} />
         <Input type='text' name='school' label='What school do you plan to attend?' />
         <TextArea
           name='study'
           label='Please take a moment to describe your intended area of study'
           placeholder='Enter details here'
-          row='4'
-          column='100'
+          row='100'
+          column='200'
         />
         <button>Submit</button>
       </Form>
--- ROOK HOTEL/2_form_validation/src/components/Select.tsx
+++ ROOK HOTEL/2_form_validation/src/components/Select.tsx
@@ -2,7 +2,23 @@ export default function Select({ name, label, register, options, ...rest }: any)
     return (
         <div className="flex flex-col">
             <label>label</label>
-            <select {...register(name)} {...rest}>
+            <select
+                className=" 
+                        form-control
+                        block
+                        w-full
+                        px-3
+                        py-1.5
+                        text-white
+                        font-normal
+                        bg-transparent bg-clip-padding
+                        border border-solid border-white
+                        rounded-sm
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-black focus:bg-white focus:border-yellow focus:outline-none"
+                {...register(name)} {...rest}>
                 {options.map((value: any) => (
                     <option key={value} value={value}>
                         {value}
